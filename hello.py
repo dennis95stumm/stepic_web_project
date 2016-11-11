9d6b3b6c85a7d7c65307b367cabf33ebf05d4965
Dennis Stumm <dstumm@yandex.ru>
2016-11-11 20:30:02 +0100

    Fix typo

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-from wsgiref.simple_server import make_server
 from urlparse import urlparse
 
 def wsgi_application(environment, start_response):
   status = '200 OK'
   headers = [('Content-Type', 'text/plain')]
-  qs = urlparse.parse_qs(envienvironmentron['QUERY_STRING'])
+  qs = urlparse.parse_qs(environment['QUERY_STRING'])
   body = ''
   for key, value in qs.iteritems():
     body += key + '=' + value + '\n'
