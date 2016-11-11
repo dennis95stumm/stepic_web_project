9f7f0443495a6c87365a4696f4b7c395c1fcf842
Dennis Stumm <dstumm@yandex.ru>
2016-11-11 20:33:14 +0100

    Update parsing of query_string

--- hello.py
+++ hello.py
@@ -3,10 +3,8 @@ from urlparse import urlparse
 def wsgi_application(environment, start_response):
   status = '200 OK'
   headers = [('Content-Type', 'text/plain')]
-  qs = urlparse.parse_qs(environment['QUERY_STRING'])
-  body = ''
-  for key, value in qs.iteritems():
-    body += key + '=' + value + '\n'
+  body = environment['QUERY_STRING'].split('&')
+  body = [item + '\r\n' for item in body]
 
   start_response(status, headers)
   return [body]
