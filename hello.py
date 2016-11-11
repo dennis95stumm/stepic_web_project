418032a772c51bdaf46d24050472c8d82c830d8a
Dennis Stumm <dstumm@yandex.ru>
2016-11-11 20:35:40 +0100

    Fix typo

--- hello.py
+++ hello.py
@@ -1,10 +1,7 @@
-from urlparse import urlparse
-
 def wsgi_application(environment, start_response):
   status = '200 OK'
   headers = [('Content-Type', 'text/plain')]
   body = environment['QUERY_STRING'].split('&')
   body = [item + '\r\n' for item in body]
-
   start_response(status, headers)
-  return [body]
+  return body
