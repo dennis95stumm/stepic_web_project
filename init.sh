333354b3ac9ea8abecdf4f1a56d983d64d432bfc
Dennis Stumm <dstumm@yandex.ru>
2016-11-11 18:08:28 +0000

    Fix init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo rm -rf /etc/nginx/sites-enabled/*
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
