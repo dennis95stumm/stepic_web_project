b473904037f6b2542f7aee728b8880352abf7b78
Dennis Stumm <dstumm@yandex.ru>
2016-11-13 20:02:55 +0000

    Fix routes config

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
   url(r'^login/$', test, name='login'),
   url(r'^signup/$', test, name='signup'),
   url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),
-  url(r'^ask/$', test, name='ask'),
+  url(r'^ask/.*$', test, name='ask'),
   url(r'^popular/$', test, name='popular'),
   url(r'^new/$', test, name='new'),
 )
