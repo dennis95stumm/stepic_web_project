941ed077f6199696db39948de03e693d2d082167
Dennis Stumm <dstumm@yandex.ru>
2016-11-13 19:53:34 +0000

    Fix urls config

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from qa.views import test
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,11 +10,11 @@ urlpatterns = patterns('',
   # url(r'^blog/', include('blog.urls')),
 
   url(r'^admin/', include(admin.site.urls)),
-  url(r'^', , name='index'),
-  url(r'^login/', 'test', name='login'),
-  url(r'^signup/', 'test', name='signup'),
-  url(r'^question/(?P<id>[0-9]+)/', 'test', name='question'),
-  url(r'^ask/', 'test', name='ask'),
-  url(r'^popular/', 'test', name='popular'),
-  url(r'^new/', 'test', name='new'),
+  url(r'^', test, name='index'),
+  url(r'^login/', test, name='login'),
+  url(r'^signup/', test, name='signup'),
+  url(r'^question/(?P<id>[0-9]+)/', test, name='question'),
+  url(r'^ask/', test, name='ask'),
+  url(r'^popular/', test, name='popular'),
+  url(r'^new/', test, name='new'),
 )
