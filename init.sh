06b692ac5db3c31a423089e788e98c2c8567455c
Dennis Stumm <dstumm@yandex.ru>
2016-11-14 18:07:31 +0000

    Add mysql configuration on init

--- init.sh
+++ init.sh
@@ -7,4 +7,8 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+sudo /etc/init.d/mysql start
+
+# mysql -u root -e "CREATE DATABASE stepik CHARACTER SET utf8;"
+# mysql -u root -e "CREATE USER 'stepik'@'localhost' IDENTIFIED BY 'stepik';"
+# mysql -u root -e "GRANT ALL ON stepik.* TO 'stepik'@'localhost';"
