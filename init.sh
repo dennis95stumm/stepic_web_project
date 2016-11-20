8c5d7cf55ab54770dae47154dd5c44bb9d2985c7
Dennis Stumm <dstumm@yandex.ru>
2016-11-20 20:33:59 +0100

    Minor

--- init.sh
+++ init.sh
@@ -3,7 +3,6 @@
 sudo rm -rf /etc/nginx/sites-enabled/*
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo rm -rf /etc/gunicorn.d/*
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
